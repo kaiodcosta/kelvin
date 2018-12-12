0787454b0fde70dfe89427ed3eb33af0ccddb928
flurt <azriantar@outlook.com>
2018-12-12 01:49:25 +0100

    testing out connection to websocket without FICS socket

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const wss = new WebSocket.Server({ server });
 
 wss.on("connection", function(ws) {
 	console.log("Connection from client established");
-	var fics = net.createConnection(5000, "freechess.org");
+	/*var fics = net.createConnection(5000, "freechess.org");
 	fics.setEncoding("utf8");
 	fics.on("data", function(data) {
 		if (ws.readyState !== WebSocket.CLOSED) {
@@ -33,7 +33,7 @@ wss.on("connection", function(ws) {
 	});
 	ws.on("error", function(err) {
 		console.log(err);
-	});
+	});*/
 });
 
 app.use(express.static(__dirname + '/../build'));
